77f0f0253cfcddac0e13ae537443da329b285ef5
ThEhOlLoWWW <dryounes2005@gmail.com>
2026-09-18 18:08:17 +0100

    updates the Master Array

--- src/data.js
+++ src/data.js
@@ -31,4 +31,176 @@ export let apprenants = [
       },
     ],
   },
+  {
+    id: 3,
+    nomComplet: "Younes Safsafi",
+    ville: "Oujda",
+    resultats: [
+      {
+        jour: 1,
+        exercicesTermines: 20,
+        totalExercices: 20,
+        challengeTermine: true,
+      },
+      {
+        jour: 2,
+        exercicesTermines: 19,
+        totalExercices: 20,
+        challengeTermine: true,
+      },
+    ],
+  },
+  {
+    id: 4,
+    nomComplet: "Amina Script",
+    ville: "Oujda",
+    resultats: [
+      {
+        jour: 1,
+        exercicesTermines: 15,
+        totalExercices: 20,
+        challengeTermine: false,
+      },
+    ],
+  },
+  {
+    id: 5,
+    nomComplet: "Karim Algo",
+    ville: "Berkane",
+    resultats: [
+      {
+        jour: 1,
+        exercicesTermines: 10,
+        totalExercices: 20,
+        challengeTermine: false,
+      },
+      {
+        jour: 2,
+        exercicesTermines: 18,
+        totalExercices: 20,
+        challengeTermine: true,
+      },
+    ],
+  },
+  {
+    id: 6,
+    nomComplet: "Fatima Web",
+    ville: "Nador",
+    resultats: [
+      {
+        jour: 1,
+        exercicesTermines: 20,
+        totalExercices: 20,
+        challengeTermine: true,
+      },
+      {
+        jour: 2,
+        exercicesTermines: 20,
+        totalExercices: 20,
+        challengeTermine: true,
+      },
+      {
+        jour: 3,
+        exercicesTermines: 20,
+        totalExercices: 20,
+        challengeTermine: true,
+      },
+    ],
+  },
+  {
+    id: 7,
+    nomComplet: "Anass Byte",
+    ville: "Tanger",
+    resultats: [
+      {
+        jour: 1,
+        exercicesTermines: 5,
+        totalExercices: 20,
+        challengeTermine: false,
+      },
+    ],
+  },
+  {
+    id: 8,
+    nomComplet: "Zineb Stack",
+    ville: "Oujda",
+    resultats: [
+      {
+        jour: 1,
+        exercicesTermines: 17,
+        totalExercices: 20,
+        challengeTermine: true,
+      },
+      {
+        jour: 2,
+        exercicesTermines: 12,
+        totalExercices: 20,
+        challengeTermine: false,
+      },
+    ],
+  },
+  {
+    id: 9,
+    nomComplet: "Omar Css",
+    ville: "Al Hoceima",
+    resultats: [
+      {
+        jour: 1,
+        exercicesTermines: 14,
+        totalExercices: 20,
+        challengeTermine: false,
+      },
+    ],
+  },
+  {
+    id: 10,
+    nomComplet: "Salma Node",
+    ville: "Nador",
+    resultats: [
+      {
+        jour: 1,
+        exercicesTermines: 19,
+        totalExercices: 20,
+        challengeTermine: true,
+      },
+      {
+        jour: 3,
+        exercicesTermines: 16,
+        totalExercices: 20,
+        challengeTermine: false,
+      },
+    ],
+  },
+  {
+    id: 11,
+    nomComplet: "Ilyas Git",
+    ville: "Oujda",
+    resultats: [
+      {
+        jour: 1,
+        exercicesTermines: 20,
+        totalExercices: 20,
+        challengeTermine: true,
+      },
+      {
+        jour: 2,
+        exercicesTermines: 8,
+        totalExercices: 20,
+        challengeTermine: false,
+      },
+    ],
+  },
+  {
+    id: 12,
+    nomComplet: "Nadia Logic",
+    ville: "Berkane",
+    resultats: [
+      {
+        jour: 2,
+        exercicesTermines: 15,
+        totalExercices: 20,
+        challengeTermine: true,
+      },
+    ],
+  },
 ];
